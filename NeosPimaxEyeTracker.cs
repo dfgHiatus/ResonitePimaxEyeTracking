55fec53e47fa7a0d41908a241b2130d37691b889
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2021-11-27 21:02:01 -0600

    Update NeosPimaxEyeTracker.cs

--- NeosPimaxEyeTracker.cs
+++ NeosPimaxEyeTracker.cs
@@ -8,34 +8,36 @@ using WebSocketSharp.Server;
 
 namespace NeosPimaxEyeTracker
 {
-    public static class DependencyManager
-    {
-        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
-        private static extern IntPtr LoadLibrary(string lpFileName);
-
-        public static bool Init() => LoadDLL("VRCPimaxEyeTracker.PimaxEyeTracker", "PimaxEyeTracker.dll");
-
-        private static bool LoadDLL(string resourcePath, string dllName)
-        {
-            var dllPath = Path.Combine(Path.GetTempPath(), dllName);
-
-            using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath + "." + dllName))
-            {
-                if (resource == null) return false;
-
-                using (Stream dllFile = File.Create(dllPath))
-                {
-                    resource.Seek(0, SeekOrigin.Begin);
-                    resource.CopyTo(dllFile);
-                }
-            }
-
-            return LoadLibrary(dllPath) != IntPtr.Zero;
-        }
-    }
 
     public static class NeosPimaxIntegration
     {
+        public static WebSocketServer wssv;
+
+        public class WebSocketClass : WebSocketBehavior
+        {
+            // Empty string from Neos
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("[Network Message] Recieved: " +
+                    ((String.IsNullOrEmpty(e.Data)) ? "(Nothing.)" : e.Data));
+                Console.WriteLine("[Network Message] Sent: " +
+                    UpdateExpressionParameters());
+                Send(UpdateExpressionParameters());
+            }
+
+            public static void MakeServer()
+            {
+                wssv = new WebSocketServer("ws://localhost:4649");
+                wssv.AddWebSocketService<WebSocketClass>("/WebSocketClass");
+                wssv.Start();
+            }
+
+            public void SendNeosMessage()
+            {
+                Send(UpdateExpressionParameters());
+            }
+        }
+
         public static Pimax.EyeTracking.EyeTracker eyeTracker;
         public static bool isChangingActiveStatus = false;
         public static bool needsExpressionUpdate = false;
@@ -46,38 +48,52 @@ namespace NeosPimaxEyeTracker
 
         public static void OnApplicationStart()
         {
-            DependencyManager.Init();
+            Console.WriteLine("[Info] Starting...");
             eyeTracker = new Pimax.EyeTracking.EyeTracker();
             eyeTracker.OnUpdate += OnEyeTrackerUpdate;
             eyeTracker.OnStart += OnEyeTrackerStart;
             eyeTracker.OnStop += OnEyeTrackerStop;
+            // Start returns true...
+            Console.WriteLine("[Info] Eye Tracking Initialized: " + eyeTracker.Start());
+            Console.WriteLine("[Current Tracking Status] " + (eyeTracker?.Active ?? false).ToString());
+            Console.WriteLine("[Info] Starting Websocket Server...");
+            WebSocketClass.MakeServer();
+            Console.WriteLine("[Info] Websocket Server created.");
+            Console.WriteLine("");
         }
 
         public static void OnApplicationQuit()
         {
-            if (eyeTracker?.Active ?? false) eyeTracker.Stop();
+            Console.WriteLine("[Info] Quitting...");
+            if (eyeTracker?.Active ?? false)
+                eyeTracker.Stop();
         }
 
         public static void OnEyeTrackerStart()
         {
-            Console.Write("Eye Tracker Started");
+            Console.WriteLine("[Info] Eye Tracker Started");
             isChangingActiveStatus = false;
         }
 
         public static void OnEyeTrackerStop()
         {
-            Console.Write("Eye Tracker Stopped");
+            Console.WriteLine("[Info] Eye Tracker Stopped");
             isChangingActiveStatus = false;
         }
 
         public static void OnEyeTrackerUpdate()
         {
+            Console.WriteLine("[Info] Eye Tracker Updated");
+            Console.WriteLine(UpdateExpressionParameters());
+            // Send to websocket here?
             needsExpressionUpdate = true;
         }
 
         public static string UpdateExpressionParameters()
         {
-            if (eyeTracker?.Active ?? false)
+            Console.WriteLine("[Current Tracking Status] " + NeosPimaxIntegration.eyeTracker?.Active.ToString());
+
+            if (NeosPimaxIntegration.eyeTracker?.Active ?? false)
             {
                 message = "";
 
@@ -103,12 +119,12 @@ namespace NeosPimaxEyeTracker
 
                 // EyesXCombinedGaze
                 message += "[" + (NeosPimaxIntegration.eyeTracker.RightEye.Expression.Blink ?
-                                    eyeTracker.LeftEye.Expression.PupilCenter.Item1.ToString() :
-                                    eyeTracker.RightEye.Expression.PupilCenter.Item1.ToString()) + "],";
+                                    NeosPimaxIntegration.eyeTracker.LeftEye.Expression.PupilCenter.Item1.ToString() :
+                                    NeosPimaxIntegration.eyeTracker.RightEye.Expression.PupilCenter.Item1.ToString()) + "],";
                 // EyesYCombinedGaze
-                message += "[" + (NeosPimaxIntegration.eyeTracker.RightEye.Expression.Blink ? 
-                                    eyeTracker.LeftEye.Expression.PupilCenter.Item2.ToString() : 
-                                    eyeTracker.RightEye.Expression.PupilCenter.Item2.ToString()) + "],";
+                message += "[" + (NeosPimaxIntegration.eyeTracker.RightEye.Expression.Blink ?
+                                    NeosPimaxIntegration.eyeTracker.LeftEye.Expression.PupilCenter.Item2.ToString() :
+                                    NeosPimaxIntegration.eyeTracker.RightEye.Expression.PupilCenter.Item2.ToString()) + "],";
                 
                 return message;
             }
@@ -119,54 +135,46 @@ namespace NeosPimaxEyeTracker
             }
             
         }
-    }
 
-    public class NeosMain
-    {
-        public static WebSocketServer wssv;
-
-        public class WebSocketClass : WebSocketBehavior
+        public class NeosMain
         {
-            // Empty string from Neos
-            protected override void OnMessage(MessageEventArgs e)
+            public void Main(string[] args)
             {
-                Send(NeosPimaxIntegration.UpdateExpressionParameters());
-            }
+                // Console.WriteLine();
+                Console.WriteLine("Press Esc to terminate, Press Enter to view Eye Tracking Data.");
+                AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+                OnApplicationStart();
 
-            public static void MakeServer()
-            {
-                wssv = new WebSocketServer("ws://localhost:4649");
-                wssv.AddWebSocketService<WebSocketClass>("/WebSocketClass");
-                wssv.Start();
-            }
-        }
+                // Moved Websocket stuff to
 
-        public static void Main(string[] args)
-        {
-            // Console.WriteLine();
-            Console.WriteLine("Press Esc to terminate.");
-            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
-            NeosPimaxIntegration.OnApplicationStart();
-
-            WebSocketClass.MakeServer();
-            while (wssv.IsListening)
-            {
-                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                while (wssv.IsListening)
                 {
-                    // Will close program
-                    wssv.Stop();
-                }
-                Thread.Sleep(200);
-            }    
-        }
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    {
+                        // Will close program    
+                        wssv.Stop();
+                    }
+                    else
+                    {
 
-        public static void OnProcessExit(object sender, EventArgs e)
-        {
-            NeosPimaxIntegration.OnApplicationQuit();
-            if (NeosPimaxIntegration.eyeTracker?.Active ?? false) 
-                NeosPimaxIntegration.eyeTracker.Stop();
-            if (wssv.IsListening) 
-                wssv.Stop();
+                        // Otherwise output our values
+                        Console.WriteLine("[Info] Values: " +
+                            UpdateExpressionParameters());
+                    }
+                    Thread.Sleep(200);
+
+                }
+            }
+
+            public void OnProcessExit(object sender, EventArgs e)
+            {
+                OnApplicationQuit();
+                if (eyeTracker?.Active ?? false)
+                    eyeTracker.Stop();
+                if (wssv.IsListening)
+                    wssv.Stop();
+            }
         }
     }
+
 }
