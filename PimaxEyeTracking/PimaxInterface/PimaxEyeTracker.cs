8b44a362a36d098ed69ec8ad0ec509a0ab7fa205
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2021-12-01 23:36:49 -0600

    even more

--- PimaxEyeTracking/PimaxInterface/PimaxEyeTracker.cs
+++ PimaxEyeTracking/PimaxInterface/PimaxEyeTracker.cs
@@ -1,10 +1,8 @@
 ﻿using System.Runtime.InteropServices;
 using BaseX;
 
-namespace Pimax.EyeTracking
-{
-	public enum EyeParameter
-	{
+namespace Pimax.EyeTracking {
+	public enum EyeParameter {
 		GazeX, // Gaze point on the X axis (not working!)
 		GazeY, // Gaze point on the Y axis (not working!)
 		GazeRawX, // Gaze point on the X axis before smoothing is applied (not working!)
@@ -27,50 +25,46 @@ namespace Pimax.EyeTracking
 		PupilMinorUnitDiameter, // Pupil minor axis diameter, measured in millimeters
 		Blink, // Blink state (not working!)
 		Openness, // How open the eye is - 100 (closed), 50 (partially open, unreliable), 0 (open)
-		UpperEyelid, // Upper eyelid state (not working!)
+        UpperEyelid, // Upper eyelid state (not working!)
 		LowerEyelid // Lower eyelid state (not working!)
 	}
 
-	public enum EyeExpression
-	{
+	public enum EyeExpression {
 		PupilCenterX, // Pupil center on the X axis, smoothed and normalized between -1 (looking left) ... 0 (looking forward) ... 1 (looking right)
 		PupilCenterY, // Pupil center on the Y axis, smoothed and normalized between -1 (looking up) ... 0 (looking forward) ... 1 (looking down)
 		Openness, // How open the eye is, smoothed and normalized between 0 (fully closed) ... 1 (fully open)
 		Blink // Blink, 0 (not blinking) or 1 (blinking)
 	}
 
-	public enum Eye
-	{
+	public enum Eye {
 		Any,
 		Left,
 		Right
 	}
 
-	public enum CallbackType
-	{
+	public enum CallbackType {
 		Start,
 		Stop,
 		Update
 	}
 
-	public struct EyeExpressionState
-	{
-		public Eye Eye { get; private set; }
-		public float2 PupilCenter { get; private set; }
-		public float Openness { get; private set; }
-		public bool Blink { get; private set; }
+    public struct EyeExpressionState
+    {
+        public Eye Eye { get; private set; }
+        public float2 PupilCenter { get; private set; }
+        public float Openness { get; private set; }
+        public bool Blink { get; private set; }
 
-		public EyeExpressionState(Eye eyeType, EyeTracker eyeTracker)
-		{
-			this.Eye = eyeType;
-			this.PupilCenter = new float2(eyeTracker.GetEyeExpression(this.Eye, EyeExpression.PupilCenterX), eyeTracker.GetEyeExpression(this.Eye, EyeExpression.PupilCenterY));
-			this.Openness = eyeTracker.GetEyeExpression(this.Eye, EyeExpression.Openness);
-			this.Blink = (eyeTracker.GetEyeExpression(this.Eye, EyeExpression.Blink) != 0.0f);
-		}
-	}
+        public EyeExpressionState(Eye eyeType, EyeTracker eyeTracker)
+        {
+            this.Eye = eyeType;
+            this.PupilCenter = new float2(eyeTracker.GetEyeExpression(this.Eye, EyeExpression.PupilCenterX), eyeTracker.GetEyeExpression(this.Eye, EyeExpression.PupilCenterY));
+            this.Openness = eyeTracker.GetEyeExpression(this.Eye, EyeExpression.Openness);
+            this.Blink = (eyeTracker.GetEyeExpression(this.Eye, EyeExpression.Blink) != 0.0f);
+        }
+    }
 
-	public struct EyeState
-	{
+    public struct EyeState {
 		public Eye Eye { get; private set; }
 		public float2 Gaze { get; private set; }
 		public float2 GazeRaw { get; private set; }
@@ -88,10 +82,9 @@ namespace Pimax.EyeTracking
 		public float Openness { get; private set; }
 		public float UpperEyelid { get; private set; }
 		public float LowerEyelid { get; private set; }
-		public EyeExpressionState Expression { get; private set; }
+        public EyeExpressionState Expression { get; private set; }
 
-		public EyeState(Eye eyeType, EyeTracker eyeTracker)
-		{
+        public EyeState(Eye eyeType, EyeTracker eyeTracker) {
 			this.Eye = eyeType;
 			this.Gaze = new float2(eyeTracker.GetEyeParameter(this.Eye, EyeParameter.GazeX), eyeTracker.GetEyeParameter(this.Eye, EyeParameter.GazeY));
 			this.GazeRaw = new float2(eyeTracker.GetEyeParameter(this.Eye, EyeParameter.GazeRawX), eyeTracker.GetEyeParameter(this.Eye, EyeParameter.GazeRawY));
@@ -109,20 +102,19 @@ namespace Pimax.EyeTracking
 			this.LowerEyelid = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.LowerEyelid);
 			this.Openness = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.Openness);
 			this.PupilCenter = new float2(eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterX), eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterY));
-			this.Expression = new EyeExpressionState(eyeType, eyeTracker);
+            this.Expression = new EyeExpressionState(eyeType, eyeTracker);
 
-			// Convert range from 0...1 to -1...1, defaulting eyes to center (0) when unavailable
-			float x = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterX);
-			float y = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterY);
-			this.PupilCenter = new float2(x <= float.Epsilon ? 0.0f : (x * 2.0f - 1.0f), y <= float.Epsilon ? 0.0f : (y * 2.0f - 1.0f));
-			this.Openness = (x <= float.Epsilon && y <= float.Epsilon) ? 0.0f : 1.0f;
-		}
+            // Convert range from 0...1 to -1...1, defaulting eyes to center (0) when unavailable
+            float x = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterX);  
+            float y = eyeTracker.GetEyeParameter(this.Eye, EyeParameter.PupilCenterY);
+            this.PupilCenter = new float2(x <= float.Epsilon ? 0.0f : (x * 2.0f - 1.0f), y <= float.Epsilon ? 0.0f : (y * 2.0f - 1.0f));
+            this.Openness = (x <= float.Epsilon && y <= float.Epsilon) ? 0.0f : 1.0f;
+        }
 	}
 
 	public delegate void EyeTrackerEventHandler();
 
-	public class EyeTracker
-	{
+	public class EyeTracker {
 		[DllImport("PimaxEyeTracker", EntryPoint = "RegisterCallback")] private static extern void _RegisterCallback(CallbackType type, EyeTrackerEventHandler callback);
 		[DllImport("PimaxEyeTracker", EntryPoint = "Start")] private static extern bool _Start();
 		[DllImport("PimaxEyeTracker", EntryPoint = "Stop")] private static extern void _Stop();
@@ -142,16 +134,15 @@ namespace Pimax.EyeTracking
 		private EyeTrackerEventHandler _OnUpdateHandler = null;
 
 		public EyeState LeftEye { get; private set; }
-		public EyeState RightEye { get; private set; }
-		public EyeState RecommendedEye { get; private set; }
+        public EyeState RightEye { get; private set; }
+        public EyeState RecommendedEye { get; private set; }
 
-		public System.Int64 Timestamp => _GetTimestamp();
+        public System.Int64 Timestamp => _GetTimestamp();
 		//public Eye RecommendedEye => _GetRecommendedEye();
 
 		public bool Active => _IsActive();
 
-		public bool Start()
-		{
+		public bool Start() {
 			_OnStartHandler = _OnStart;
 			_RegisterCallback(CallbackType.Start, _OnStartHandler);
 
@@ -169,14 +160,12 @@ namespace Pimax.EyeTracking
 		public float GetEyeParameter(Eye eye, EyeParameter param) => _GetEyeParameter(eye, param);
 		public float GetEyeExpression(Eye eye, EyeExpression expression) => _GetEyeExpression(eye, expression);
 
-		private void _OnUpdate()
-		{
-			if (Active)
-			{
+		private void _OnUpdate() {
+			if(Active) {
 				LeftEye = new EyeState(Eye.Left, this);
-				RightEye = new EyeState(Eye.Right, this);
-				RecommendedEye = new EyeState(_GetRecommendedEye(), this);
-				OnUpdate?.Invoke();
+                RightEye = new EyeState(Eye.Right, this);
+                RecommendedEye = new EyeState(_GetRecommendedEye(), this);
+                OnUpdate?.Invoke();
 			}
 		}
 
@@ -184,17 +173,14 @@ namespace Pimax.EyeTracking
 		private void _OnStop() => OnStop?.Invoke();
 	}
 
-	public class PimaxEyeTracker
-	{
+	public class PimaxEyeTracker {
 		public EyeTracker EyeTracker;
 
-		void Awake()
-		{
+		void Awake() {
 			EyeTracker = new EyeTracker();
 		}
 
-		void OnDestroy()
-		{
+		void OnDestroy() {
 			if (EyeTracker.Active) EyeTracker.Stop();
 		}
 	}
